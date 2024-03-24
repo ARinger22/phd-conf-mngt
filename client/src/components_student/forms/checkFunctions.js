e4f25590d98136fed7823b8c8ab9aaff59ebd61f
ARinger22 <gautamarpit1204@gmail.com>
2024-03-25 02:05:26 +0530

    updated forms

--- client/src/components_student/forms/checkFunctions.js
+++ client/src/components_student/forms/checkFunctions.js
@@ -45,14 +45,8 @@ export function checkConfAndLeaveTime(sc, ec, sl, el) {
     const etc = new Date(ec);
     const slc = new Date(sl);
     const elc = new Date(el);
-
-    // console.log(stc);
-    // console.log(etc);
-    // console.log(stc - slc >= 0);
-    // console.log(slc);
-    // console.log(elc);
-    // console.log(elc - etc >= 0);
-    if ((stc - slc >= 0) && (elc - etc >= 0)) {
+    
+    if ((stc - slc >= 0) && (etc - elc >= 0)) {
         return true;
     }
     window.alert("Please Check your Conference and Duty Leave Time");
