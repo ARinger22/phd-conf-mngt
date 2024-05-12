8e5c03f8630c0e3b1ee8615d7b22e7b66c6a0145
Arker123 <kharbandarnav@gmail.com>
2024-05-12 15:20:35 +0530

    Lots of features

--- client/src/components/team/Team.js
+++ client/src/components/team/Team.js
@@ -8,6 +8,7 @@ import tanuj from './images/tanuj.jpg';
 import arnav from './images/arnav.jpeg';
 import arpit from './images/arpit.jpeg';
 import vanshika from './images/vanshika.jpeg';
+import simran from './images/simran.jpeg'
 import './Team.css';
 import { Link } from 'react-router-dom';
 import Footer from '../../components_student/Side/Footer';
@@ -94,7 +95,7 @@ function Team() {
             <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
               <img
                 className="mx-auto h-[26rem] w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src={adish}
+                src={simran}
                 alt="Simran Kaur"
                 loading="lazy"
                 width="640"
